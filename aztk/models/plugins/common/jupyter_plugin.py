af03e7d3807f87a8714173273f33178862f5ce08
Timothee Guerin <timothee.guerin@outlook.com>
2018-02-13 18:52:42 -0800

    Move plugins

--- aztk/models/plugins/common/jupyter_plugin.py
+++
@@ -1,7 +0,0 @@
-from aztk.models.plugins.plugin_configuration import PluginConfiguration
-
-class JupyterPlugin(PluginConfiguration):
-    def __init__(self):
-        super().__init__(
-            name="jupyter",
-        )
